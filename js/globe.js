fbff7ed29e2a87dac25ccfebdd193e7756bb916f
PAPORALKIS <k.paporalkis@gmail.com>
2025-05-25 00:14:02 +0200

    Update globe.js

--- js/globe.js
+++ js/globe.js
@@ -1,13 +1,5 @@
 console.log("globe.js chargé");
 
-const textures = [
-  'img/meuble-laura-1.jpg',
-  'img/Iso2.jpg',
-  'img/ext1.jpg',
-  'img/CUIEXT5.jpg',
-  'img/SDB3.jpg',
-  'img/DRESSMARS1.jpg'
-];
 const canvas = document.getElementById("globeCanvas");
 const renderer = new THREE.WebGLRenderer({ canvas, alpha: true });
 renderer.setPixelRatio(window.devicePixelRatio);
@@ -19,12 +11,38 @@ const camera = new THREE.PerspectiveCamera(
   0.1,
   1000
 );
-camera.position.z = 3;
+camera.position.z = 4;
 
+// Sphère centrale
 const sphereGeometry = new THREE.SphereGeometry(1.2, 32, 32);
 const sphereMaterial = new THREE.MeshNormalMaterial({ wireframe: true });
 const globe = new THREE.Mesh(sphereGeometry, sphereMaterial);
 scene.add(globe);
+
+// Images à orbiter autour du globe
+const textures = [
+  'img/meuble-laura-1.jpg',
+  'img/Iso2.jpg',
+  'img/ext1.jpg',
+  'img/CUIEXT5.jpg',
+  'img/SDB3.jpg',
+  'img/DRESSMARS1.jpg'
+];
+
+const loader = new THREE.TextureLoader();
+const orbitRadius = 2.2;
+const orbitingImages = [];
+
+textures.forEach((imgSrc, index) => {
+  loader.load(imgSrc, texture => {
+    const material = new THREE.MeshBasicMaterial({ map: texture, transparent: true });
+    const geometry = new THREE.PlaneGeometry(0.8, 0.6);
+    const imageMesh = new THREE.Mesh(geometry, material);
+
+    scene.add(imageMesh);
+    orbitingImages.push({ mesh: imageMesh, angle: (index / textures.length) * Math.PI * 2 });
+  });
+});
 
 function resizeRendererToDisplaySize() {
   const width = canvas.clientWidth * window.devicePixelRatio;
@@ -48,6 +66,15 @@ function animate() {
 
   globe.rotation.y += 0.002;
   globe.rotation.x += 0.001;
+
+  // Rotation des images
+  orbitingImages.forEach((obj, i) => {
+    obj.angle += 0.002;
+    const x = orbitRadius * Math.cos(obj.angle);
+    const y = orbitRadius * Math.sin(obj.angle);
+    obj.mesh.position.set(x, y, 0);
+    obj.mesh.lookAt(0, 0, 0);
+  });
 
   renderer.render(scene, camera);
 }
