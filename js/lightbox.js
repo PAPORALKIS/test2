07862324bdefae4ffdf8ed108a973e2306cefa98
PAPORALKIS <k.paporalkis@gmail.com>
2025-05-25 23:32:08 +0200

    Update lightbox.js

--- js/lightbox.js
+++ js/lightbox.js
@@ -2,24 +2,31 @@ document.addEventListener("DOMContentLoaded", function () {
   const zoomables = document.querySelectorAll('.realisation-image');
   const lightbox = document.getElementById('lightbox');
   const lightboxImg = document.getElementById('lightbox-img');
+  const lightboxDesc = document.getElementById('lightbox-description');
 
   zoomables.forEach(img => {
     img.addEventListener('click', (e) => {
       e.stopPropagation();
       lightboxImg.src = img.src;
+
+      const card = img.closest('.card');
+      const description = card ? card.querySelector('.description')?.innerHTML : '';
+      lightboxDesc.innerHTML = description;
+
       lightbox.style.display = 'flex';
     });
   });
 
   lightbox.addEventListener('click', function () {
-    lightbox.style.display = 'none';
-    lightboxImg.src = '';
+    closeLightbox();
   });
 });
 
 function closeLightbox() {
   const lightbox = document.getElementById('lightbox');
   const lightboxImg = document.getElementById('lightbox-img');
+  const lightboxDesc = document.getElementById('lightbox-description');
   lightbox.style.display = 'none';
   lightboxImg.src = '';
+  lightboxDesc.innerHTML = '';
 }
