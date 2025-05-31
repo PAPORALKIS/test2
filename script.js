2f7602d0728ec1cd3ca7608155edf16f4b86883c
PAPORALKIS <k.paporalkis@gmail.com>
2025-06-01 00:53:57 +0200

    Update script.js

--- script.js
+++ script.js
@@ -51,12 +51,12 @@ const baseRadius = getAdaptiveRadius(planes.length);
 let distance;
 
 if (isPortrait) {
-if (window.innerWidth < 480) distance = baseRadius + 10;
-else if (window.innerWidth < 768) distance = baseRadius + 8;
-else distance = baseRadius + 6;
+if (window.innerWidth < 480) distance = baseRadius + 30;
+else if (window.innerWidth < 768) distance = baseRadius + 30;
+else distance = baseRadius + 30;
 } else {
-if (window.innerWidth < 768) distance = baseRadius + 8;
-else if (window.innerWidth < 1024) distance = baseRadius + 6;
+if (window.innerWidth < 768) distance = baseRadius + 30;
+else if (window.innerWidth < 1024) distance = baseRadius + 30;
 else distance = baseRadius + 5;
 }
 
